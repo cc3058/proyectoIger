56de7ce4a9a87e1f12561169b121f0ea6a81bdc5
val17102 <val17102@uvg.edu.gt>
2019-05-03 14:43:55 -0600

    iniciar

--- client/src/services/AuthenticationService.js
+++ client/src/services/AuthenticationService.js
@@ -3,7 +3,10 @@ import Api from '@/services/Api'
 export default {
   signup (credentials) {
     return Api().post('register', credentials)
+  },
+
+  login(credentials){
+    console.log(credentials)
+    return Api().post('login',credentials)
   }
 }
-
-
