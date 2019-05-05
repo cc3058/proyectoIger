1df104db871ec2f626551ce7ecb19423a014444d
rmonzon98 <35532598+rmonzon98@users.noreply.github.com>
2019-05-05 15:58:26 -0600

    save circle

--- client/src/components/CrearCirculo.vue
+++ client/src/components/CrearCirculo.vue
@@ -2,13 +2,16 @@
     <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Información de ubicación</h2>
-    <v-container fluid grid-list-xl>
+    <v-container fluid grid-list-md>
       <v-layout wrap align-center>
         <div class="inner">
-          <v-select class="drop-down-conf" v-model="sector" label="Sector" :items="sectores" ></v-select>
-          <v-select class="drop-down-conf" v-model="region" label="Región" :items="sectores" ></v-select>
-          <v-text-field label = "Dirección" v-model="name"></v-text-field>
-          <v-text-field label = "Código" v-model="number"></v-text-field>
+          <v-text-field v-model="dept" label="Departamento"></v-text-field>
+          <v-text-field v-model="municipio" label="Municipio"></v-text-field>
+          <v-text-field label = "Número de círculo" v-model="number"></v-text-field>
+          <v-text-field label = "Dirección" v-model="direccion"></v-text-field>
+          <v-text-field label = "Coordinador" v-model="coordinador"></v-text-field>
+          <v-text-field label = "Encargado" v-model="encargado"></v-text-field>
+          <v-textarea label = "Orientador (si es más de uno, separarlos por comas)" v-model="orientador"></v-textarea>
           <v-btn depressed dark class="orange font-weight-bold" v-on:click="creado()">Crear círculo</v-btn> <v-btn depressed dark class="grey font-weight-bold" v-on:click="cancelar()">Cancelar</v-btn>
         </div>
       </v-layout>
@@ -19,25 +22,47 @@
 
 <script>
 import router from '../router'
+import Api from '@/services/Api'
 
     export default {
         name: 'UserType',
         data () {
             return {
                 msg: 'Crear Circulo',
-                name: '',
-                sector: '',
-                region: '',
-                number: ''
+                direccion: '',
+                dept: '',
+                municipio: '',
+                number: '',
+                coordinador: '',
+                encargado: '',
+                orientador: ''
             }
         },
         methods: {
             creado() {
+                const post = Api().post
 
-                router.push({ name: "AdminHome" });
+                const data = {
+                    departamento: this.dept,
+                    municipio: this.municipio,
+                    circulo: this.number,
+                    coordinador: this.coordinador,
+                    encargado: this.encargado,
+                    orientador: this.orientador.split(',')
+                }
+                
+                post(
+                    'circulos/add',
+                    data
+                ).catch( error => {
+                    console.log(error)
+                    return null
+                }).then(response => {
+                    console.log(data)
+                    router.push({ name: "AdminHome" })
+                })
             },
             cancelar() {
-
                 router.push({ name: "AdminHome" });
             }
           }
@@ -49,21 +74,17 @@ import router from '../router'
     h1, h2 {
         font-weight: normal;
     }
-
     ul {
         list-style-type: none;
         padding: 0;
     }
-
     li {
         display: inline-block;
         margin: 0 10px;
     }
-
     a {
         color: #42b983;
     }
-
     .inner {
       display: table;
       margin: 0 auto;
