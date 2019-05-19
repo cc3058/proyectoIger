140db6c59888bd4d8eb0c9a3f1ca4aceaa4a687f
Andrea Arguello <arg17801@uvg.edu.gt>
2019-05-18 18:56:52 -0600

    Ver info del círculo (frontend)

--- client/src/components/InfoCirculo.vue
+++ client/src/components/InfoCirculo.vue
@@ -1,17 +1,22 @@
 <template>
     <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Información de ubicación</h2>
     <v-container fluid grid-list-md>
       <v-layout row wrap align-center>
         <div class="inner">
-          <v-select class="drop-down-conf" v-model="circulo" label="Seleccione el círculo que desea ver" :items="circles" ></v-select>
-          <v-text-field label = "Orientadores" v-model="orientadores" readonly></v-text-field>
-          <v-textarea
-          name="input-7-1"
-          label="Información" readonly
-        ></v-textarea>
-        
+          <v-select class="drop-down-conf" v-model="circulo" label="Seleccione el círculo que desea ver" id = "circulo" :items="circles" ></v-select>
+          <v-text-field label = "Orientadores" readonly id ="orientadores"></v-text-field>
+          <br>
+          <h3><small>Estudiantes</small></h3>
+          <b-pagination
+            align="center"
+            v-model="currentPage"
+            :total-rows="rows"
+            :per-page="perPage"
+            aria-controls="my-table"
+            ></b-pagination>
+            <p class="mt-3">Página actual: {{ currentPage }}</p>
+            <b-table striped hover :fields="fields" :items="items" :per-page="perPage" :current-page="currentPage"></b-table>
           <v-btn depressed dark class="orange font-weight-bold" v-on:click="creado()">Enviar</v-btn> <v-btn depressed dark class="grey font-weight-bold" v-on:click="cancelar()">Cancelar</v-btn>
         </div>
       </v-layout>
@@ -27,12 +32,37 @@ import router from '../router'
         name: 'UserType',
         data () {
             return {
-                msg: 'Enviar planificación',
-                orientadores: '',
+                msg: 'Información de círculo',
                 circulo: '',
-                region: '',
-                number: '',
-                circles: ['Aca se jalaria de la db']
+                circles: ['Aca se jalaria de la db'],
+                //De la tabla
+                perPage: 5,
+                currentPage: 1,
+                fields: [
+                    {
+                        key: 'last_name',
+                        label: 'Apellido',
+                        sortable: true
+                    },
+                    {
+                        key: 'first_name',
+                        label: 'Nombre',
+                        sortable: false
+                    },
+                    {
+                        key: 'grado',
+                        label: 'Grado',
+                        sortable: true,
+                        // Variant applies to the whole column, including the header and footer
+                        //variant: 'danger'
+                    }
+                ],
+                items: [
+                    // Para efectos didácticos pero debería ser desde la db
+                    { grado: 4, first_name: 'Dickerson', last_name: 'Macdonald' },
+                    { grado: 5, first_name: 'Larsen', last_name: 'Shaw' },
+                    { grado: 'Retirado', first_name: 'Geneva', last_name: 'Wilson' },
+                ]
             }
         },
         methods: {
@@ -44,7 +74,12 @@ import router from '../router'
 
                 router.push({ name: "AdminHome" });
             }
-          }
+          },
+        computed: {
+            rows() {
+            return this.items.length
+            }
+        }
     }
 </script>
 
